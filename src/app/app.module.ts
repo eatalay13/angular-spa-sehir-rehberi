2a859b86c93f3eec8485c5c8a83d55e3d817b45e
Emrah Atalay <emrahatalay92@gmail.com>
2019-06-16 15:00:59 +0300

    city ve city detail componentleri eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
+import { CityDetailComponent } from './city/city-detail/city-detail.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      CityComponent,
+      CityDetailComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [],
    bootstrap: [
---
+++ src/app/services/city.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { City } from '../models/city';
+import { Photo } from '../models/photo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityService {
+
+  constructor(private http: HttpClient) { }
+
+  path = 'https://localhost:44330/api/';
+
+  getCities(): Observable<City[]> {
+    return this.http.get<City[]>(this.path + 'cities');
+  }
+
+  getCityById(cityId: number): Observable<City> {
+    return this.http.get<City>(this.path + 'cities/' + cityId);
+  }
+
+  getPhotosByCity(cityId: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>(this.path + 'cities/photos/?cityId=' + cityId);
+  }
+}
